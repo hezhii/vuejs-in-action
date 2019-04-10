cc0152ff9068a33fc10c7d2d80edc8ed58b81863
biu <hezhou920@gmail.com>
2019-04-10 13:08:23 +0800

    在 update 中支持表达式的更新

--- DropMenu/clickoutside.js
+++ DropMenu/clickoutside.js
@@ -12,6 +12,21 @@ Vue.directive('clickoutside', {
     el.__vueClickOutside__ = documentHandler
     document.addEventListener('click', documentHandler)
   },
+  update(el, binding) {
+    if (binding.oldValue !== binding.value) {
+      function documentHandler(e) {
+        if (el.contains(e.target)) {
+          return false
+        }
+        if (binding.expression) {
+          binding.value(e)
+        }
+      }
+      document.removeEventListener('click', el.__vueClickOutside__)
+      el.__vueClickOutside__ = documentHandler
+      document.addEventListener('click', documentHandler)
+    }
+  },
   unbind(el) {
     document.removeEventListener('click', el.__vueClickOutside__)
     delete el.__vueClickOutside__
